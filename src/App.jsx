8e773a51092ed96673e366681d46befef3c6ad20
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-10-25 15:30:49 +0200

    Completed interactive background

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Calculator from './Calculator';
+import InteractiveBackground from './InteractiveBackground';
 
 function App() {
   return (
     <>
+      <InteractiveBackground />  
       <h1>Calculator with 
           <span className='cool'>
-            <span className='cool-text'> cool css effects</span>
+            <span className='cool-text'> Interactive background</span>
           </span>
-        </h1>
+      </h1>
       <Calculator />
     </>
   );
---
+++ src/InteractiveBackground.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import './InteractiveBackground.css';
+
+const InteractiveBackground = () => {
+    let interBubble = document.querySelector('.interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+      if (interBubble) {
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+      }
+      requestAnimationFrame(() => {
+        move();
+      });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+      tgX = event.clientX;
+      tgY = event.clientY;
+    });
+
+    move();
+
+
+  return (
+    <div className="interactive-background">
+      <div className="gradient-bg">
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                result="goo"
+              />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="gradients-container">
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+          <div className="g4"></div>
+          <div className="g5"></div>
+          <div className="interactive"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InteractiveBackground;
+
