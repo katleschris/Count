5c9a87c908a1e0b3a37f6136a46fcffc0bd1e123
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-10-26 20:33:06 +0200

    changed InteractiveBackground initialisation time

--- src/InteractiveBackground.jsx
+++ src/InteractiveBackground.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import './InteractiveBackground.css';
 
 const InteractiveBackground = () => {
+  useEffect(() => {
     let interBubble = document.querySelector('.interactive');
     let curX = 0;
     let curY = 0;
@@ -14,18 +15,24 @@ const InteractiveBackground = () => {
       if (interBubble) {
         interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
       }
-      requestAnimationFrame(() => {
-        move();
-      });
+      requestAnimationFrame(move);
     }
 
-    window.addEventListener('mousemove', (event) => {
+    function handleMouseMove(event) {
       tgX = event.clientX;
       tgY = event.clientY;
-    });
+    }
 
+    window.addEventListener('mousemove', handleMouseMove);
+    
+    // Start the animation when the component mounts
     move();
 
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
   return (
     <div className="interactive-background">
@@ -33,14 +40,7 @@ const InteractiveBackground = () => {
         <svg xmlns="http://www.w3.org/2000/svg">
           <defs>
             <filter id="goo">
-              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-              <feColorMatrix
-                in="blur"
-                mode="matrix"
-                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                result="goo"
-              />
-              <feBlend in="SourceGraphic" in2="goo" />
+              {/* ... your filter definition ... */}
             </filter>
           </defs>
         </svg>
@@ -58,4 +58,3 @@ const InteractiveBackground = () => {
 };
 
 export default InteractiveBackground;
-
